ae53d73c54064fac4f4bacc92983e3a21ae3ff88
gitk47320 <j.kashi.37204.privateuse@gmail.com>
2021-01-23 16:47:29 +0900

    readonly

---
+++ src/read-only.ts
@@ -0,0 +1,11 @@
+export {};
+
+class VisaCard {
+  //readonlyをつけるとその変数を別の値に更新することはできない
+  constructor(public readonly owner: string) {}
+}
+
+let myVisaCard = new VisaCard('はむさん')
+console.log(myVisaCard.owner)
+
+//myVisaCard.owner = 'ベーコン'
