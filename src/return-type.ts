dba9f3701a6477a28ac77c780fdf5482f13f20d9
gitk47320 <j.kashi.37204.privateuse@gmail.com>
2021-02-14 16:56:59 +0900

    infer

--- src/return-type.ts
+++ src/return-type.ts
@@ -8,3 +8,11 @@ console.log(add(1, 2));
 
 //ある関数の返り値の型を取得し、定義する
 type ReturnTypeFromAdd = ReturnType<typeof add>;
+
+//ReturnTypeの定義は以下の通り
+//inferは条件の一部としてジェネリクス型を宣言する
+type MyReturnType<T extends (...args: any) => any> = T extends (
+  ...args: any
+) => infer R
+  ? R
+  : any;
