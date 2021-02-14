df79946f133f36be7673d327b3600be70ce6eecb
gitk47320 <j.kashi.37204.privateuse@gmail.com>
2021-02-14 16:49:20 +0900

    return-type

--- src/return-type.ts
+++ src/return-type.ts
@@ -1,1 +1,10 @@
-export{};
+export {};
+
+function add(a: number, b: number) {
+  return a + b;
+}
+
+console.log(add(1, 2));
+
+//ある関数の返り値の型を取得し、定義する
+type ReturnTypeFromAdd = ReturnType<typeof add>;
